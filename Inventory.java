2fef0404e3019662447e91cebfc79f15c014611c
Laika May <66331364+ChinamaykzaLaika@users.noreply.github.com>
2022-05-02 13:45:34 -0400

    Update Inventory.java

--- Inventory.java
+++ Inventory.java
@@ -56,6 +56,9 @@ public abstract class Inventory {
     public String getTitle() {
         return title;
     }
+    public void setTitle(String title) {
+        this.title = title;
+    }
     public void setAuthorArtist (String authorArtist) {
         this.authorArtist = authorArtist;
     }
