860ea41a45cd9a0d49563e50a5ac7f0e32664011
Laika May <66331364+ChinamaykzaLaika@users.noreply.github.com>
2022-05-03 13:56:40 -0400

    Update BookstoreManagementProject2.java

--- BookstoreManagementProject2.java
+++ BookstoreManagementProject2.java
@@ -3,7 +3,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package bookstoremanagementproject2;
-
+// The program can currently take customer orders but is unable to assign total amount spent for customer, apply discounts, and print csv file after program closes
+// REMAINING REQUIREMENTS: 1) store total cost in customer info's total spent, 2) apply discount, and 3) print csv file after store closes
+// additionally monetary cost of purchase is being stored in the orderList array
 /**
  *
  * @author Paydreanne E. Hinton
@@ -28,6 +30,8 @@ public class BookstoreManagementProject2 {
         ArrayList<Book> bookList = new ArrayList<>();
         ArrayList<CD> cdList = new ArrayList<>();
         ArrayList<DVD> dvdList = new ArrayList<>();
+        ArrayList<Double> pricingOrderList = new ArrayList<>();
+        ArrayList<String> orderList = new ArrayList<>();
 
 
         Book book = new Book();
@@ -59,7 +63,7 @@ public class BookstoreManagementProject2 {
                     // object
                     Book bookz = new Book(productID, itemType, title, authorArtist, stockSize, productPrice);
                     bookList.add(bookz);
-                    bookz.productDisplay(); 
+                    // bookz.productDisplay(); 
                 }
                 else if(line.contains("cd")) { // if line contains book
 
@@ -76,7 +80,7 @@ public class BookstoreManagementProject2 {
                     // object
                     CD cdz = new CD(productID, itemType, title, authorArtist, stockSize, productPrice);
                     cdList.add(cdz);
-                    cdz.productDisplay(); 
+                    // cdz.productDisplay(); 
                 }
                 else if(line.contains("dvd")) { // if line contains book
 
@@ -93,7 +97,7 @@ public class BookstoreManagementProject2 {
                     // object
                     DVD dvdz = new DVD(productID, itemType, title, authorArtist, stockSize, productPrice);
                     dvdList.add(dvdz);
-                    dvdz.productDisplay(); 
+                    // dvdz.productDisplay(); 
                 }
             }
         //} catch (Exception e) {}
@@ -152,18 +156,129 @@ public class BookstoreManagementProject2 {
                     System.out.println("Here's the customer's information: ");
                     System.out.println("\nCustomer name: " + r.getFirstName() + " " + r.getLastName() + "\nPremium member? " + r.getIsPremiumMember() + "\nPaid monthly premium? " + r.getHasPaidPremium() + "\nTotal spent at bookstore: " + "$" + r.getTotalSpending() + "\nPreferred Payment Method: " + r.getPayMethod() + "\n");
                     
-                    // 
-                    // Display what store has on sale
+                    // DISPLAY WHAT STORE HAS ON SALE
+                    // Project 3 Starting...
                     System.out.println("What the bookstore has on sale now: ");
-                    System.out.println("1) The Art of War - $5.99 (Book)");
-                    System.out.println("2) Studio Ghibli Soundtrack - $19.99 (CD)");
-                    System.out.println("3) Up - $7.99 (DVD)");
+                    boolean continueShopping = true;
+                    while (continueShopping) {
+                    for (int i = 0; i < bookList.size(); i++) {System.out.println(bookList.get(i).getProductID() + ") \"" + bookList.get(i).getTitle() + "\" by " + bookList.get(i).getAuthorArtist() + " (Book) - $" + bookList.get(i).getProductPrice());}
+                    for (int i = 0; i < cdList.size(); i++) {System.out.println(cdList.get(i).getProductID() + ") \"" + cdList.get(i).getTitle() + "\" by " + cdList.get(i).getAuthorArtist()+ " (CD) - $" + cdList.get(i).getProductPrice());}
+                    for (int i = 0; i < dvdList.size(); i++) {System.out.println(dvdList.get(i).getProductID() + ") \"" + dvdList.get(i).getTitle() + "\" by " + dvdList.get(i).getAuthorArtist() + " (DVD) - $" + dvdList.get(i).getProductPrice());}
+                    System.out.println("Enter number beside the product to purchase");
+                    int purchaseChoice = scnr.nextInt();
+                    
+                    if (purchaseChoice >= 0 && purchaseChoice <= 10){ // WHEN PURCHASING BOOK
+                        System.out.println("How many copies of \"" + bookList.get(purchaseChoice).getTitle() + "\" would you like?");
+                        int purchaseQuantity = scnr.nextInt();
 
-                    int booksPurchased = 0;
-                    int cdsPurchased = 0;
-                    int dvdsPurchased = 0;
+                        if (purchaseQuantity > bookList.get(purchaseChoice).getStockSize() && bookList.get(purchaseChoice).getStockSize() != 0) {
+                            System.out.println("We only have " + bookList.get(purchaseChoice).getStockSize() + " copies of \"" + bookList.get(purchaseChoice).getTitle() + "\" available. Would you like to purchase the remaining copies? (y/n)");
+                            char purchaseRemaining = scnr.next().charAt(0);
+                            if (purchaseRemaining == 'Y' || purchaseRemaining == 'y') {
+                                purchaseQuantity = bookList.get(purchaseChoice).getStockSize();
+                                bookList.get(purchaseChoice).setStockSize(purchaseChoice);
+                                pricingOrderList.add((bookList.get(purchaseChoice).getProductPrice() * purchaseQuantity));
+                                orderList.add(purchaseQuantity + " \"" + bookList.get(purchaseChoice).getTitle() + "\"\t\t\t" + "\n");
+                            }
+                            else if (bookList.get(purchaseChoice).getStockSize() == 0) {
+                                purchaseQuantity = bookList.get(0).getStockSize();
+                            }
+                            else if (purchaseRemaining == 'N' || purchaseRemaining == 'n') {
+                                purchaseQuantity = 0;
+                            }
+                        }
+                        else if (purchaseQuantity <= bookList.get(purchaseChoice).getStockSize()) {
+                            bookList.get(purchaseChoice).deductStockSize(purchaseQuantity);
+                            bookList.get(purchaseChoice).setTotalPurchasePrice(purchaseQuantity);
+                            orderList.add(purchaseQuantity + " " + bookList.get(purchaseChoice).getTitle() + "\t\t\t");
+                            pricingOrderList.add((bookList.get(purchaseChoice).getProductPrice() * purchaseQuantity));
+                        }
+                        else if (bookList.get(purchaseChoice).getStockSize() == 0) {
+                            purchaseQuantity = 0;
+                            System.out.println("\nWe're sorry, \"" + bookList.get(purchaseChoice).getTitle() + "\" is currently out of stock.");
+                        }
+                        // System.out.println("Customer bought " + purchaseQuantity + " copies for total of $" + (bookList.get(purchaseChoice).getProductPrice() * purchaseQuantity) + "(original price was: " + bookList.get(purchaseChoice).getProductPrice() + ")");
+                        }
+                    else if (purchaseChoice >= 11 && purchaseChoice <= 13){ // WHEN PURCHASING CD
+                        System.out.println("How many copies of \"" + cdList.get(purchaseChoice).getTitle() + "\" would you like?");
+                        int purchaseQuantity = scnr.nextInt();
+
+                        if (purchaseQuantity > bookList.get(0).getStockSize() && bookList.get(0).getStockSize() != 0) {
+                            System.out.println("We only have " + bookList.get(0).getStockSize() + " copies of \"Fahrenheit 451\" available. Would you like to purchase the remaining copies? (y/n)");
+                            char purchaseRemaining = scnr.next().charAt(0);
+                            if (purchaseRemaining == 'Y' || purchaseRemaining == 'y') {
+                                purchaseQuantity = bookList.get(0).getStockSize();
+                                bookList.get(0).setStockSize(0);
+                            }
+                            else if (bookList.get(0).getStockSize() == 0) {
+                                purchaseQuantity = bookList.get(0).getStockSize();
+                            }
+                            else if (purchaseRemaining == 'N' || purchaseRemaining == 'n') {
+                                purchaseQuantity = 0;
+                            }
+                        }
+                        else if (purchaseQuantity <= bookList.get(0).getStockSize()) {
+                            bookList.get(0).deductStockSize(purchaseQuantity);
+                            bookList.get(0).setTotalPurchasePrice(purchaseQuantity);
+                        }
+                        else if (bookList.get(0).getStockSize() == 0) {
+                            purchaseQuantity = 0;
+                            System.out.println("\nWe're sorry, \"Fahrenheit 451\" is currently out of stock.");
+                        }
+                        System.out.println("Customer bought " + purchaseQuantity + " copies for total of $" + (bookList.get(0).getProductPrice() * purchaseQuantity) + "(original price was: " + bookList.get(0).getProductPrice() + ")");
+                        
+                    }
+                    else if (purchaseChoice >= 14 && purchaseChoice <= 15) { // WHEN PURCHASING DVD
+                        System.out.println("How many copies of \"" + dvdList.get(purchaseChoice).getTitle() + "\" would you like?");
+                        int purchaseQuantity = scnr.nextInt();
+
+                        if (purchaseQuantity > bookList.get(0).getStockSize() && bookList.get(0).getStockSize() != 0) {
+                            System.out.println("We only have " + bookList.get(0).getStockSize() + " copies of \"Fahrenheit 451\" available. Would you like to purchase the remaining copies? (y/n)");
+                            char purchaseRemaining = scnr.next().charAt(0);
+                            if (purchaseRemaining == 'Y' || purchaseRemaining == 'y') {
+                                purchaseQuantity = bookList.get(0).getStockSize();
+                                bookList.get(0).setStockSize(0);
+                            }
+                            else if (bookList.get(0).getStockSize() == 0) {
+                                purchaseQuantity = bookList.get(0).getStockSize();
+                            }
+                            else if (purchaseRemaining == 'N' || purchaseRemaining == 'n') {
+                                purchaseQuantity = 0;
+                            }
+                        }
+                        else if (purchaseQuantity <= bookList.get(0).getStockSize()) {
+                            bookList.get(0).deductStockSize(purchaseQuantity);
+                            bookList.get(0).setTotalPurchasePrice(purchaseQuantity);
+                        }
+                        else if (bookList.get(0).getStockSize() == 0) {
+                            purchaseQuantity = 0;
+                            System.out.println("\nWe're sorry, \"Fahrenheit 451\" is currently out of stock.");
+                        }
+                        System.out.println("Customer bought " + purchaseQuantity + " copies for total of $" + (bookList.get(0).getProductPrice() * purchaseQuantity) + "(original price was: " + bookList.get(0).getProductPrice() + ")");
+                        
+                    }
+                    System.out.println("Does the customer want to purchase anything else? (y/n)");
+                        char purchaseMore = scnr.next().charAt(0);
+                        if (purchaseMore == 'Y' || purchaseMore == 'y') {}
+                        else if (purchaseMore == 'N' || purchaseMore == 'n') {
+                            System.out.println("\n\nHere is " + regMem.getFirstName() + " " + regMem.getLastName() + "'s receipt: \n");
+                            for (int i = 0; i < orderList.size(); i++) {
+                                System.out.println(orderList.get(i) + "\t\t\t\t$" + pricingOrderList.get(i));
+                            }
+                            double sum = 0;
+                            for(Double d : pricingOrderList)
+                            sum += d;
+                            double discountTotal = sum * .85;
+                            r.setTotalSpending(discountTotal);
+                            System.out.println("INITIAL TOTAL \t\t\t\t$" + sum);
+                            System.out.println("FINAL TOTAL (REGULAR DISCOUNT) \t\t$" + discountTotal);
+                            continueShopping = false;
+                        }
+                }
+                    // Project 3 ending...
 
                     // BOOKS
+                    /*
                     if (book.getStockSize() > 0) {
                         System.out.println("\nWould the customer like to purchase the book \"The Art of War\"? (y/n)");
                         char confirmBookPurchase = scnr.next().charAt(0);
@@ -280,7 +395,8 @@ public class BookstoreManagementProject2 {
                     System.out.println("Given customer's regular membership discount (15% off), the amount is now $" + df.format(discountPrice));
                     r.setTotalSpending(discountPrice);
                     bookstoreBalance = bookstoreBalance + discountPrice;    
-                }
+                }*/
+            }
                 // Premium
                 else if (isPremMem == 2) {
                     System.out.println("Has this premium member paid their monthly fee yet? (y/n)");
@@ -322,15 +438,10 @@ public class BookstoreManagementProject2 {
                     
                     // Display what store has on sale
                     System.out.println("What the bookstore has on sale now: ");
-                    System.out.println("1) The Art of War - $5.99 (Book)");
-                    System.out.println("2) Studio Ghibli Soundtrack - $19.99 (CD)");
-                    System.out.println("3) Up - $7.99 (DVD)");
-
-                    int booksPurchased = 0;
-                    int cdsPurchased = 0;
-                    int dvdsPurchased = 0;
+                    
 
                     // BOOKS
+                    /*
                     if (book.getStockSize() > 0) {
                         System.out.println("\nWould the customer like to purchase the book \"The Art of War\"? (y/n)");
                         char confirmBookPurchase = scnr.next().charAt(0);
@@ -446,7 +557,7 @@ public class BookstoreManagementProject2 {
                     double discountPrice = customerTotalSpent * .6;
                     System.out.println("Given customer's premium membership discount (40% off), the amount is now $" + df.format(discountPrice));
                     p.setTotalSpending(discountPrice);
-                    bookstoreBalance = bookstoreBalance + discountPrice;
+                    bookstoreBalance = bookstoreBalance + discountPrice;*/
                 }
                 choiceComplete = true;
             }
@@ -529,5 +640,6 @@ public class BookstoreManagementProject2 {
                 System.exit(0);
             }
         }
+        
     }
 }
